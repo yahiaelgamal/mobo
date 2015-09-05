c6ae3694e11914100190ca1296d0c18e8ed9bda1
yahiaelgamal <yahiaelgamal@gmail.com>
2015-09-05 23:07:13 +0200

    Simple cleanups

--- fetch_data.py
+++ fetch_data.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 import urllib
 import re
 import json
+import sys
 
 
 # returns hash of book dom element
@@ -35,14 +36,20 @@ def get_book_info(book):
     return(book_hash)
 
 
+# USE DEFENSEIVE PROGGRAMMING
 def get_movie_info(movie):
     movie_name = movie.find_next('a').text
     movie_url = 'http://www.imdb.com' + movie.find_next('a').get('href')
-    info = movie.find_next('div', class_='rating').get('title')
-    avg_rating = re.findall('rated this ([0-9.]*)', info)[0]
-    avg_rating = float(avg_rating)
-    num_ratings = re.findall('([0-9,]*) votes', info)[0]
-    num_ratings = int(num_ratings.replace(',', ''))
+    info = movie.find_next('div', class_='rating')
+    try:
+        info = info.get('title')
+        avg_rating = re.findall('rated this ([0-9.]*)', info)[0]
+        avg_rating = float(avg_rating)
+        num_ratings = re.findall('([0-9,]*) votes', info)[0]
+        num_ratings = int(num_ratings.replace(',', ''))
+    except:
+        avg_rating = -1
+        num_ratings = -1
     published = movie.find_next('span', class_='year_type').text
     try:
         published = int(re.findall('\(([0-9]*)', published)[0])
@@ -72,20 +79,21 @@ def get_goodreads_data(base_url, pages=41):
     return books_hash
 
 
-def get_imdb_data(base_url, pages):
+def get_imdb_data(base_url, movies):
     movies_hash = []
-    for page in range(1, pages+1):
-        print('Getting info from page %s' % page)
-        r = urllib.urlopen('%s?page=%s' % (base_url, page))
+    for start in range(1, movies, 50):
+        print('Getting info from movie %s' % start)
+        r = urllib.urlopen('%s&start=%s' % (base_url, start))
+        print('%s&start=%s' % (base_url, start))
         soup = BeautifulSoup(r.read(), 'html.parser')
-        movies = soup.find_all('', class_='info')
+        movies = soup.find_all('', class_='title')
         movies_hash += [get_movie_info(movie) for movie in movies]
         print("we have %d movies so far" % len(movies_hash))
     return movies_hash
 
 
-def defaults(books=True, movies=True):
-    if(books):
+def defaults(data_points):
+    if('books' in data_points):
         book_movies_url = 'https://www.goodreads.com/shelf/show/books-made-into-movies'
         books_hash = get_goodreads_data(base_url=book_movies_url, pages=41)
         books_output_filename = 'goodreads_ratings.json'
@@ -93,13 +101,15 @@ def defaults(books=True, movies=True):
         with open(books_output_filename, 'w') as outfile:
             json.dump(books_hash, outfile)
 
-    if(movies):
-        movies_base_url = 'http://www.imdb.com/list/ls050071819/'
-        movies_hash = get_imdb_data(base_url=movies_base_url, pages=3)
+    if('movies' in data_points):
+        movies_base_url = 'http://www.imdb.com/search/title?at=0&keywords=based-on-novel&sort=moviemeter&title_type=feature&year=1990,2015'
+        movies_hash = get_imdb_data(base_url=movies_base_url, movies=4578)
         movies_output_filename = 'movies_ratings.json'
 
         with open(movies_output_filename, 'w') as outfile:
             json.dump(movies_hash, outfile)
 
+
 if __name__ == "__main__":
-    defaults(True, True)
+    print(sys.argv)
+    defaults(sys.argv)
